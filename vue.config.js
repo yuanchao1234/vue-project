6bdb06ce91a3c0274564c61ff4a8086bf8ba8449
lzg <michael_lizg@163.com>
2020-05-10 11:05:10 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -8,12 +8,10 @@ function resolve (dir) {
   return path.join(__dirname, dir)
 }
 module.exports = {
-  // 部署应用包时的基本URL，默认为'/'
-  // 假设你的应用将会部署在域名的根部,比如，https://www.vue-cli.com/,则设置为"/"
-  // 如果你的应用是部署在一个子路径下，那么你需要在这里指定子路径，比如，如果你部署在 https://www.my-vue.com/my-app/; 那么将这个值改为 “/my-app/”
+
   publicPath: './',
 
-  // 将构建好的文件输出到哪里 当运行 vue-cli-service build 时生成的生产环境构建文件的目录。注意目标目录在构建之前会被清除 (构建时传入 --no-clean 可关闭该行为)。
+  // 将构建好的文件输出到哪里
   outputDir: 'dist/static',
 
   // 放置生成的静态资源(js、css、img、fonts)的目录。
@@ -22,21 +20,13 @@ module.exports = {
   // 指定生成的 index.html 的输出路径
   indexPath: 'index.html',
 
-  // 默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存。然而，这也要求 index 的 HTML 是被 Vue CLI 自动生成的。如果你无法使用 Vue CLI 生成的 index HTML，你可以通过将这个选项设为 false 来关闭文件名哈希
-  filenameHashing: true,
-
-  // 是否在开发环境下通过 eslint-loader 在每次保存时 lint 代码。这个值会在 @vue/cli-plugin-eslint 被安装之后生效。
-  // 设置为 true 时，eslint-loader 会将 lint 错误输出为编译警告。默认情况下，警告仅仅会被输出到命令行，且不会使得编译失败。
-  // 如果你希望让 lint 错误在开发时直接显示在浏览器中，你可以使用 lintOnSave: 'error'。这会强制 eslint-loader 将 lint 错误输出为编译错误，同时也意味着 lint 错误将会导致编译失败。
-  // lintOnSave: false,
-
   // 是否使用包含运行时编译器的 Vue 构建版本。设置为 true 后你就可以在 Vue 组件中使用 template 选项了，但是这会让你的应用额外增加 10kb 左右。
   runtimeCompiler: false,
 
   // 默认情况下 babel-loader 会忽略所有 node_modules 中的文件。如果你想要通过 Babel 显式转译一个依赖，可以在这个选项中列出来。
   transpileDependencies: [],
 
-  // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+  // 生产环境关闭 source map
   productionSourceMap: false,
 
   // lintOnSave: true,
@@ -49,7 +39,6 @@ module.exports = {
     // css预设器配置项
     loaderOptions: {
       postcss: {
-        // options here will be passed to postcss-loader
         plugins: [
           require('postcss-px2rem')({
             remUnit: 100
@@ -113,9 +102,6 @@ module.exports = {
   // https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa
   pwa: {},
 
-  // 所有 webpack-dev-server 的选项都支持。注意：有些值像 host、port 和 https 可能会被命令行参数覆写。
-  // 有些值像 publicPath 和 historyApiFallback 不应该被修改，因为它们需要和开发服务器的 publicPath 同步以保障正常的工作。
-  // 代理配置
   devServer: {
     host: '0.0.0.0',
     port: 8088, // 端口号
