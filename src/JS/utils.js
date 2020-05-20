c4c239ee51f95c694179a3c7b4407f23a8ed7226
lzg <398055960@qq.com>
2020-05-20 11:13:07 +0800

    update

--- src/JS/utils.js
+++ src/JS/utils.js
@@ -282,7 +282,7 @@ export default {
       },
       // 防抖
       _debounce (fn, delay) {
-        var delay = delay || 200
+        var delay1 = delay || 200
         var timer
         return function () {
           var th = this
@@ -293,14 +293,14 @@ export default {
           timer = setTimeout(function () {
             timer = null
             fn.apply(th, args)
-          }, delay)
+          }, delay1)
         }
       },
       // 节流
       _throttle (fn, interval) {
         var last
         var timer
-        var interval = interval || 200
+        var interval1 = interval || 200
         return function () {
           var th = this
           var args = arguments
@@ -310,7 +310,7 @@ export default {
             timer = setTimeout(function () {
               last = now
               fn.apply(th, args)
-            }, interval)
+            }, interval1)
           } else {
             last = now
             fn.apply(th, args)
